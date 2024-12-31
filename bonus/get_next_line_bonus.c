048e07894cff2bac44af0c70e823a5bf8cb683ca
DexterxD <soualaymanehamdan2@gmail.com>
2024-12-31 23:57:28 +0100

    updating the bonus

--- bonus/get_next_line_bonus.c
+++ bonus/get_next_line_bonus.c
@@ -6,59 +6,72 @@
 /*   By: sohamdan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 09:03:49 by sohamdan          #+#    #+#             */
-/*   Updated: 2024/12/31 15:29:59 by sohamdan         ###   ########.fr       */
+/*   Updated: 2024/12/31 23:55:25 by sohamdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static char	*extract_line(char *buffer)
+static char	*line_extract(char **buffer)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
-	if (!buffer)
+	line = NULL;
+	if (!(*buffer))
 		return (NULL);
-	while (buffer && buffer[i] != '\n')
+	while (*buffer && (*buffer)[i] != '\n')
 		i++;
-	if (buffer[i] == '\n')
-	{
-		line = ft_strndup(buffer, i + 1);
-		buffer = ft_strndup(ft_strchr(buffer, '\n'), 0);
-	}
+	if ((*buffer)[i] == '\n')
+		line = ft_strndup(*buffer, i + 1);
 	else
 	{
-		line = ft_strndup(buffer, 0);
-		buffer = NULL;
+		line = ft_strndup(*buffer, 0);
+		free(*buffer);
+		*buffer = NULL;
 	}
 	return (line);
+}
+
+int	line_read(char **buffer, int fd)
+{
+	char	*temp;
+	char	*temp_buffer;
+	int		bytes_read;
+
+	temp = ft_strndup(ft_strchr(*buffer, '\n'), 0);
+	free(*buffer);
+	*buffer = temp;
+	while (!ft_strchr(*buffer, '\n'))
+	{
+		temp_buffer = (char *)malloc(BUFFER_SIZE + 1);
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(temp_buffer);
+			break ;
+		}
+		temp_buffer[bytes_read] = '\0';
+		*buffer = ft_strjoin(*buffer, temp_buffer);
+		free(temp_buffer);
+	}
+	return (bytes_read);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer[1024];
-	char		*newline;
-	char		*temp_buffer;
-	int			read_bytes;
+	int			bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	temp_buffer = (char *)malloc(BUFFER_SIZE + 1);
-	if (!buffer[fd])
-		buffer[fd] = (char *)malloc(BUFFER_SIZE);
-	newline = NULL;
-	buffer[fd] = ft_strchr(buffer[fd], '\n');
-	while (newline == NULL)
+	bytes_read = line_read(&buffer[fd], fd);
+	if (bytes_read <= 0 && buffer[fd][0] == '\0')
 	{
-		read_bytes = read(fd, temp_buffer, BUFFER_SIZE);
-		if (read_bytes <= 0)
-			break ;
-		temp_buffer[read_bytes] = '\0';
-		buffer[fd] = ft_strjoin(buffer[fd], temp_buffer);
-		newline = ft_strchr(buffer[fd], '\n');
-	}
-	if (read_bytes < 0 || (read_bytes == 0 && buffer[fd][0] == '\0'))
+		free(buffer[fd]);
+		buffer[fd] = NULL;
 		return (NULL);
-	return (extract_line(buffer[fd]));
+	}
+	return (line_extract(&buffer[fd]));
 }
